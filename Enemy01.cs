e00370a53f6c0943946bb15953e29a0616c85494
Eduardo DdC <eduardo-dcs@outlook.es>
2018-10-23 01:40:42 -0500

    Add files via upload

---
+++ Enemy01.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy01 : MonoBehaviour {
+    public Transform player;
+    static Animator anim;
+
+	
+	void Start () {
+
+        anim = GetComponent<Animator>();
+    }
+	
+	
+	void Update () {
+       
+        if(Vector3.Distance(player.position, this.transform.position) < 10)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                                        Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("isIdle", false);
+            if(direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 0.05f);
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAttacking", false);
+            }
+            else
+            {
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isWalking", false);
+             }
+        }
+        else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", false);
+        }
+	}
+}
---
+++ Interpolar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interpolar : MonoBehaviour {
+
+    public Transform[] targets;
+    public int currenTargetIndex;
+    public float speed;
+    
+    
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position, 
+            targets[currenTargetIndex].position, 
+            speed * Time.deltaTime);
+      
+
+        float dist = Vector3.Distance(transform.position, targets[currenTargetIndex].position);
+        if (dist <= 0.1)
+        {
+            currenTargetIndex++;
+            if(currenTargetIndex <= targets.Length)
+            {
+                currenTargetIndex = 0;
+            }
+        }
+    }
+}
